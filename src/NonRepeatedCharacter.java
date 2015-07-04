f1420b5296d61bb95638c065fe4cba9ef43468c2
NataBurtnyk <nataliaburtnyk@gmail.com>
2015-07-04 13:30:49 +0300

    refactoring

--- src/NonRepeatedCharacter.java
+++ src/NonRepeatedCharacter.java
@@ -2,22 +2,23 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Test {
+public class NonRepeatedCharacter {
 
 	public static void main(String[] args) {
 
 		System.out.println("Enter youre text:");
 		String sc = new Scanner(System.in).nextLine();
-		char c = firstNonRepeatedCharacter(sc);
-		System.out.println("The first non repeated character is: " + c);
+		NonRepeatedCharacter c = new NonRepeatedCharacter();
+	
+		System.out.println("VALUE = "+ sc);
+		System.out.println("RESULT = " + c.firstNonRepeatedCharacter(sc));
 	}
 
-	private static char firstNonRepeatedCharacter(String str) {
+	public char firstNonRepeatedCharacter(String str) {
 		HashMap<Character, Integer> characterhashtable = new HashMap<Character, Integer>();
-		Character c;
 		char result = 0;
 		for (int i = 0; i < str.length(); i++) {
-			c = str.charAt(i);
+			Character c = str.charAt(i);
 			if (characterhashtable.containsKey(c)) {
 				characterhashtable.put(c, characterhashtable.get(c) + 1);
 			} else {
@@ -26,12 +27,15 @@ public class Test {
 		}
 
 		for (int i = 0; i < str.length(); i++) {
-			c = str.charAt(i);
+			Character c = str.charAt(i);
 			if (characterhashtable.get(c) == 1) {
 				result = c;
+			}else if(characterhashtable.get(c) == 0){
+				result = 0;
 			}
 		}
 		return result;
 	}
 
 }
+ 
